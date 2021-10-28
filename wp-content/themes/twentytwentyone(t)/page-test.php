987e0a8c4a2c5f524240d07568e3d696a4b37a12
kaiyu li <krisfk@gmail.com>
2021-10-29 04:40:40 +0800

    .

--- wp-content/themes/twentytwentyone(t)/page-test.php
+++ wp-content/themes/twentytwentyone(t)/page-test.php
@@ -24,7 +24,7 @@
         <?php
        
        $args = array(  
-        'post_type' => 'Studentt',
+        'post_type' => 'Student',
         'post_status' => 'publish',
         'posts_per_page' => 8, 
         'orderby' => 'title', 
