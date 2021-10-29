5c7c953ae4845ee886b8209227b273f42165fc10
kaiyu li <krisfk@gmail.com>
2021-10-29 15:04:49 +0800

    .

--- wp-content/themes/twentytwentyone(t)/myscript.js
+++ wp-content/themes/twentytwentyone(t)/myscript.js
@@ -1,2 +1,10 @@
 var $ = jQuery;
-console.log($('body'));
+
+$('body')
+    .children()
+    .each(function() {
+        $(this).html($(this).html().replaceAll('NT', '新界'));
+        $(this).html($(this).html().replaceAll('HK', '香港'));
+        $(this).html($(this).html().replaceAll('KL', '九龍'));
+        // $(this).html($(this).html().replaceAll('NT', '新界'));
+    });
