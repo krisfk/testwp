803c1005d23f11c5bfe52b1f74980b91af5961be
kaiyu li <krisfk@gmail.com>
2021-10-29 15:05:47 +0800

    .

--- wp-content/themes/twentytwentyone(t)/myscript.js
+++ wp-content/themes/twentytwentyone(t)/myscript.js
@@ -1,10 +1,13 @@
 var $ = jQuery;
 
-$('body')
-    .children()
-    .each(function() {
-        $(this).html($(this).html().replaceAll('NT', '新界'));
-        $(this).html($(this).html().replaceAll('HK', '香港'));
-        $(this).html($(this).html().replaceAll('KL', '九龍'));
-        // $(this).html($(this).html().replaceAll('NT', '新界'));
-    });
+$(function() {
+    $('body')
+        .children()
+        .each(function() {
+            $(this).html($(this).html().replaceAll('NT', '新界'));
+            $(this).html($(this).html().replaceAll('HK', '香港'));
+            $(this).html($(this).html().replaceAll('KL', '九龍'));
+
+            // $(this).html($(this).html().replaceAll('NT', '新界'));
+        });
+});
