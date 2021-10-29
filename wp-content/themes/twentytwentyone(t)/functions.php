8b0da3a9ed2e21454268493862f2f271ecad143a
kaiyu li <krisfk@gmail.com>
2021-10-29 14:03:29 +0800

    .

--- wp-content/themes/twentytwentyone(t)/functions.php
+++ wp-content/themes/twentytwentyone(t)/functions.php
@@ -662,3 +662,14 @@ function wpc_custom_term_name( $term_name, $e_name ){
 
     return $term_name;
 }
+
+function my_enqueue($hook) {
+    // Only add to the edit.php admin page.
+    // See WP docs.
+    if ('edit.php' !== $hook) {
+        return;
+    }
+    wp_enqueue_script('my_custom_script', plugin_dir_url(__FILE__) . '/myscript.js');
+}
+
+add_action('admin_enqueue_scripts', 'my_enqueue');
