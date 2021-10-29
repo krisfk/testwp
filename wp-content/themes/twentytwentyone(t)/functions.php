237f0671125811532c4270de451e43e6dfab4ca1
kaiyu li <krisfk@gmail.com>
2021-10-29 15:10:15 +0800

    .

--- wp-content/themes/twentytwentyone(t)/functions.php
+++ wp-content/themes/twentytwentyone(t)/functions.php
@@ -663,6 +663,11 @@ function my_enqueue($hook) {
     if ('edit.php' !== $hook  ) {
         return;
     }
+
+	if ('post.php' !== $hook  ) {
+        return;
+    }
+	
     wp_enqueue_script('my_custom_script', get_template_directory_uri(). '/myscript.js');
 
 }
