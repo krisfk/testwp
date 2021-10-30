3b28b34d59f0f8ed9bf5ae89fd5edacb45095254
kaiyu li <krisfk@gmail.com>
2021-10-31 04:55:55 +0800

    .

--- wp-content/themes/twentytwentyone(t)/page-test.php
+++ wp-content/themes/twentytwentyone(t)/page-test.php
@@ -1,28 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+get_header();
 
-<body>
-    <!-- fdfdsafa -->
+/* Start the Loop */
+while ( have_posts() ) :
+	the_post();
+	get_template_part( 'template-parts/content/content-page' );
 
-    <?php  ?>
-    <?php echo do_shortcode('[fe_widget  id="24" show_selected="yes" ] '); ?>
+	// If comments are open or there is at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) {
+		comments_template();
+	}
+endwhile; // End of the loop.
 
-
-    <!-- [fe_widget title="Filters" id="26" show_selected="yes" show_count="yes"] -->
-
-    <!-- <div id="testing"></div> -->
-
-    <div id="primary">
-
-
-    </div>
-</body>
-
-</html>
+get_footer();
