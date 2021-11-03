a2b7a2f534a9f2a079f87c8691137372bc4ee4d6
kaiyu li <krisfk@gmail.com>
2021-11-04 00:34:35 +0800

    .

--- wp-content/themes/twentytwentyone(t)/functions.php
+++ wp-content/themes/twentytwentyone(t)/functions.php
@@ -650,16 +650,16 @@ function wpc_custom_term_name( $term_name, $e_name ){
 	
 
 
-    if( $e_name === 'level' && $term_name === "K1"  ){
-        $term_name = '中文';
-    }
-	if( $e_name === 'level' && $term_name === "K2"  ){
-        $term_name = '英文';
-    }
+    // if( $e_name === 'level' && $term_name === "K1"  ){
+    //     $term_name = '中文';
+    // }
+	// if( $e_name === 'level' && $term_name === "K2"  ){
+    //     $term_name = '英文';
+    // }
 
-	if( $e_name === 'level' && $term_name === "K3"  ){
-        $term_name = '數學';
-    }
+	// if( $e_name === 'level' && $term_name === "K3"  ){
+    //     $term_name = '數學';
+    // }
     // if( $e_name === 'subject' && $term_name === "English"  ){
     //     $term_name = '英文';
     // }
@@ -668,7 +668,7 @@ function wpc_custom_term_name( $term_name, $e_name ){
     //     $term_name = '數學';
     // }
 
-    return $term_name;
+    // return $term_name;
 }
 
 
