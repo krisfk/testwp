fc9e6446da17477062cc622a171526e1c430baf0
kaiyu li <krisfk@gmail.com>
2021-11-03 23:00:32 +0800

    .

--- wp-content/themes/twentytwentyone(t)/archive.php
+++ wp-content/themes/twentytwentyone(t)/archive.php
@@ -30,11 +30,13 @@ $description = get_the_archive_description();
     <?php the_post(); ?>
     <?php get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) ); ?>
     <?php endwhile; ?>
+
+
 </div>
-<?php twenty_twenty_one_the_posts_navigation(); ?>
+<?php //twenty_twenty_one_the_posts_navigation(); ?>
 
 <?php else : ?>
-<?php get_template_part( 'template-parts/content/content-none' ); ?>
+<?php //get_template_part( 'template-parts/content/content-none' ); ?>
 <?php endif; ?>
 
 <?php get_footer(); ?>
